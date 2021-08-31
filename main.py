2bdaf313223fa1a21333e3e497359555079acc13
adam paxton <adam@paxton.me.uk>
2021-08-31 23:32:56 +0100

    Strings
    Works with strings

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-brand: str = "Amigoscode"
-isAdult: bool = False
-
-# def hello() -> str: this would force the returned value to be a string
-def hello():
-    return "Hello"
+brand = 'Amigoscode'
+print(brand.replace('A','a'))
+print(len(brand))
+print(brand == 'Amigoscode')
+print('code' not in brand)
