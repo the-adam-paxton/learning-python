7b98377741ed6bec8d32a620b7d64494bcb0d29d
adam paxton <adam@paxton.me.uk>
2021-08-31 23:41:34 +0100

    string part 2
    More string work, mainly multiline stuff

--- main.py
+++ main.py
@@ -3,3 +3,20 @@ print(brand.replace('A','a'))
 print(len(brand))
 print(brand == 'Amigoscode')
 print('code' not in brand)
+
+name = "Jamila"
+email = """
+Hello {},
+How are you? 
+It was nice talking to you
+"""
+
+print(email.format(name))
+
+age = 22
+note = f"""
+Hello {name},
+You never told me you are {age}!
+"""
+
+print(note)
