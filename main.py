4bf5a0f8b1a07fd4abdc550b6e905dc2d522c9cc
adam paxton <adam@paxton.me.uk>
2021-09-01 06:44:56 +0100

    Arithmetic Operators
    Basic arithmetic operators - mod and power included

--- main.py
+++ main.py
@@ -1,22 +1,5 @@
-brand = 'Amigoscode'
-print(brand.replace('A','a'))
-print(len(brand))
-print(brand == 'Amigoscode')
-print('code' not in brand)
-
-name = "Jamila"
-email = """
-Hello {},
-How are you? 
-It was nice talking to you
-"""
-
-print(email.format(name))
-
-age = 22
-note = f"""
-Hello {name},
-You never told me you are {age}!
-"""
-
-print(note)
+print(1 + 2)
+print(2 * 2)
+print(10 / 2)
+print(9 % 2) #Gives the remainder
+print (3 ** 2) #3 to the power 2
