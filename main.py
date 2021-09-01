cfc697ecd28f4be687135ba7520201dae011ed8e
adam paxton <adam@paxton.me.uk>
2021-09-01 14:22:41 +0100

    Logical Operators
    AND, OR and NOT examples

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-print(10 > 5)
-print(10 <= 10)
-print(10 != 5)
-print(10 == 10)
-print(2 > 3)
-# really quick change
+print((10 > 5) and (10 % 2 == 0) and "A" == "A")
+print(10 <= 10 or 2 > 3)
+print(not("James" == "Maria"))
