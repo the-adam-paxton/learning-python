c3f5f954cb85dda1d3cb7345d9ba13084ec40855
adam paxton <adam@paxton.me.uk>
2021-09-01 22:08:28 +0100

    Deleting from a list
    Using pop and del to remove items from a list

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-numbers = [1, 2, 3, 4, -1, 0, -20]
+numbers = [1, 2, 3, 4, -1, 0, -20, 1]
 
 #numbers.sort()
 #numbers.reverse()
 #numbers.append(1000)
 #print(len(numbers))
 #numbers.clear()
-print(4 in numbers)
+#print(4 in numbers)
+
+numbers.pop(1)
+del numbers[1:2] #[1,3,4,-1,0,-20,1] as 2 was removed by the previous pop, starting from 1 remove upto BUT NOT INC 2 = [1,4,-1,0,-20,1]
+print(numbers)
