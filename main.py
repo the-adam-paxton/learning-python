d0e8187925397d018298f6918dae1f8bb42c1ddc
adam paxton <adam@paxton.me.uk>
2021-09-01 22:30:55 +0100

    union, intersect and difference comment
    Added a comment to explain these only work with sets

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 lettersA = {"A", "B", "C", "D"}
 lettersB = {"A", "E", "F"}
+
+# This can only be done with sets {} and fails if you try this with lists []
 union = lettersA | lettersB  # Add them together, order not guaranteed
 intersection = lettersA & lettersB  # What occurs in both
 difference = lettersA - lettersB  # What occurs in lettersA but not in lettersB
