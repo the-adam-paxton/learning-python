d151eed50c74c62e536a2e01a0ebaaf83bf7cb2d
adam paxton <adam@paxton.me.uk>
2021-09-01 22:14:33 +0100

    Sets
    Basic intro to sets

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-numbers = [1, 2, 3, 4, -1, 0, -20, 1]
+numbersList = [1, 2, 3, 4, -1, 0, -20, 1]
+numbersSet = {1, 1}
+lettersSet = {"A", "A", "B", "C"}  # no duplicates output and order is not guaranteed
 
-#numbers.sort()
-#numbers.reverse()
-#numbers.append(1000)
-#print(len(numbers))
-#numbers.clear()
-#print(4 in numbers)
-
-numbers.pop(1)
-del numbers[1:2] #[1,3,4,-1,0,-20,1] as 2 was removed by the previous pop, starting from 1 remove upto BUT NOT INC 2 = [1,4,-1,0,-20,1]
-print(numbers)
+print(numbersList)
+print(numbersSet)
+print(lettersSet)
