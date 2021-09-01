84490cecdca793381fd0fcb8d78ae7c693feb0c3
adam paxton <adam@paxton.me.uk>
2021-09-01 15:43:44 +0100

    IF Statements
    Quick IF statement

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-number = 2
-number += 2
-print(number)
-print(number)
+number = 15
+
+if number > 0:
+    print(f"Number {number} is positive")
+elif number == 0:
+    print("Number is Zero")
+else:
+    print(f"Number {number} is Negative")
