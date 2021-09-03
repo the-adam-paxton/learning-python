932a83305a0b06ab4bdeda209c9211b2d67c93d3
adam paxton <adam@paxton.me.uk>
2021-09-03 09:38:47 +0100

    creating files
    Learning to read, write and append to file

--- main.py
+++ main.py
@@ -1,19 +1,7 @@
-from datetime import datetime
-from datetime import date
-
-print(datetime.today())
-print(datetime.now())
-print(datetime.now().year)
-print(datetime.now().month)
-print(datetime.now().day)
-print(datetime.now().time())
-
-
-now = datetime.now()
-print(datetime.now())
-
-print(now.strftime("%d-%m-%Y %H:%M:%S"))
-print(now.strftime("%d-%B-%Y %H:%M:%S"))
-print(now.strftime("%d-%b-%Y %H:%M:%S"))
-
-print(date.today().strftime("%d-%m-%Y"))
+# w = writing, a = appending, r = reading, r+ = reading and writing
+file = open("./data.csv", "a")
+file.write("id,name,email\n")
+file.write("1,Jamila,jamila@gmail.com\n")
+file.write("2,Adam,adam@gmail.com\n")
+file.write("3,Steve,steve@gmail.com\n")
+file.close()
