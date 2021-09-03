47e036a2cb0e8fa38e5039b21e03fdab8974dcfa
adam paxton <adam@paxton.me.uk>
2021-09-03 10:37:41 +0100

    Fetching jokes from the internet
    Good lesson fetching data, creating an object and for loops

--- main.py
+++ main.py
@@ -1,5 +1,31 @@
 from urllib import request
+import json
 
-r = request.urlopen("http://www.google.com")
-print(r.getcode())
-print(r.read())
+url = "https://official-joke-api.appspot.com/random_ten"
+r = request.urlopen(url)
+data = r.read()
+jsonData = json.loads(data)
+# print(r.getcode())
+# print(jsonData)
+
+class Joke:
+    def __init__(self, setup, punchline):
+        self.setup = setup
+        self.punchline = punchline
+
+    def __str__(self):
+        return f"Setup: {self.setup}\nPunchline: {self.punchline}\n\n"
+
+
+jokes = []
+
+for j in jsonData:
+    setup = j["setup"]
+    punchline = j["punchline"]
+    joke = Joke(setup, punchline)
+    jokes.append(joke)
+
+print(f"I found {len(jokes)} jokes")
+
+for joke in jokes:
+    print(joke)
